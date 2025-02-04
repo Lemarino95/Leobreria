3fb2770e50bd6a5950e9dcc57f7a7755db39db32
Leonardo Marino <leo-marino@hotmail.it>
2025-02-04 15:33:53 +0100

    Update ft_putendl_fd.c

--- main folder/libft/ft_putendl_fd.c
+++ main folder/libft/ft_putendl_fd.c
@@ -1,7 +1,14 @@
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd)
+size_t	ft_putendl_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
-	write(fd, "\n", 1);
+	size_t	i;
+
+	i = 0;
+	if (fd)
+	{
+		i += write(fd, s, ft_strlen(s));
+		i += write(fd, "\n", 1);
+	}
+	return (i);
 }
