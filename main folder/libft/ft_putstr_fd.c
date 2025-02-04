fb6afb0ea68ac7e1f6f6c03d27bb62afab789967
Leonardo Marino <leo-marino@hotmail.it>
2025-02-04 15:36:51 +0100

    Update ft_putstr_fd.c

--- main folder/libft/ft_putstr_fd.c
+++ main folder/libft/ft_putstr_fd.c
@@ -1,6 +1,11 @@
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+size_t	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	if (fd)
+	{
+		return (write(fd, s, ft_strlen(s)));
+	}
+	else
+		return (0);
 }
