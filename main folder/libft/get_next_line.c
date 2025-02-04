02aa3ccf70fd1fc7f0ba9cf27d31202824272bbf
Leonardo Marino <leo-marino@hotmail.it>
2025-02-04 15:28:14 +0100

    Update get_next_line.c

--- main folder/libft/get_next_line.c
+++ main folder/libft/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "libft.h"
 
 static char	*ft_appenduntil(const char *src, int c)
 {
