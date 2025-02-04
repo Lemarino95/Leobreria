3c3ff37cf4f13a3050a8d32e526834d07374d851
Leonardo Marino <leo-marino@hotmail.it>
2025-02-04 15:28:33 +0100

    Update ft_printf.c

--- main folder/libft/ft_printf.c
+++ main folder/libft/ft_printf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
 int	ft_convert(va_list arg, const char format)
 {
