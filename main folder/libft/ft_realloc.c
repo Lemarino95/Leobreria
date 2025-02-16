a58f17b0f1b9ad376e45101a52017e8dbdb50a71
Leonardo Marino <leo-marino@hotmail.it>
2025-02-16 13:02:12 +0100

    Update ft_realloc.c

--- main folder/libft/ft_realloc.c
+++ main folder/libft/ft_realloc.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 /* Changes the size of the memory block pointed to by ptr to size bytes.
 The contents will be unchanged in the range from the start of the region up to the
@@ -19,20 +19,16 @@ memory  will  not  be  initialized.   If  ptr is NULL, then the call is equivale
 loc(size), for all values of size; if size is equal to zero, and ptr is not NULL, then the
 call  is  equivalent  to  free(ptr).
 Unless ptr is NULL, it must have been re turned by an earlier call to malloc(), calloc(), or realloc().*/
-void	*ft_realloc(void *ptr, size_t size)
+void		*ft_realloc(void *ptr, size_t size)
 {
-	size_t	i;
-	void	*nptr;
+	void	*new_ptr;
 
-	i = 0;
-	nptr = (void *)malloc(size);
-	if (!nptr)
-		return (NULL);
-	while (((char *)ptr)[i])
-		{
-			((char *)nptr)[i] = ((char *)ptr)[i];
-			i++;
-		}
+	if (ptr == NULL)
+		return (malloc(size));
+	if (!size)
+		return (ptr);
+	new_ptr = malloc(size);
+	ft_memcpy(new_ptr, ptr, size);
 	free(ptr);
-	return (nptr);
+	return (new_ptr);
 }
