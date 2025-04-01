77cac4a5cd55f96602f322c1f1073f51e97ee6a4
Leonardo Marino <leo-marino@hotmail.it>
2025-04-01 11:30:01 +0200

    Update placehold.h

--- main folder/placehold.h
+++ main folder/placehold.h
@@ -1,6 +1,44 @@
 #ifndef PLACEHOLD_H
 # define PLACEHOLD_H
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MACROS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~COLORS~~~~~~~~~~~~~~~*/
+# define NO_ALL "\033[0m"
+
+# define NO_COLOR "\e[39m"
+# define BLACK "\e[30m"
+# define RED "\033[31m"
+# define GREEN "\e[32m"
+# define BRGREEN "\033[32;1m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[95m"
+# define BRCYAN "\033[96m"
+
+/*~~~~~~~~~~~~BACKGROUNDS~~~~~~~~~~~~~*/
+# define BGMAGENTA "\033[45m"
+# define NO_BG "\e[49m"
+
+/*~~~~~~~~~~~~~~~MORE~~~~~~~~~~~~~~~*/
+# define BOLD "\e[1m"// "\e[1;4m" Bold and Underlined
+# define NO_BOLD "\e[21m"
+
+# define UNDERLINE "\e[4m"
+# define NO_UNDERLINE "\e[24m"
+
+# define BLINK "\e[5m"//Doesn't work on vscode terminal
+# define NO_BLINK "\e[25m"
+
+# define HIDE "\e[8m"
+# define NO_HIDE "\e[28m"
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~STRUCTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
 
 # include "libft.h"
 
